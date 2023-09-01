bce779dfca045ba693fb1fb133679677b278d1c4
David Navarro <daveanavarro@icloud.com>
2023-08-31 19:06:29 -0600

    Search bar and filters

--- base/views.py
+++ base/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    query = request.GET.get('query') if request.GET.get('query') != None else ''
+    rooms = Room.objects.filter(
+        Q(host__username__icontains=query) |
+        Q(topic__name__icontains=query) |
+        Q(name__icontains=query) |
+        Q(description__icontains=query) 
+        )
+    room_count = rooms.count()
+    topics = Topic.objects.all()
+    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)
 
 
@@ -31,10 +40,18 @@ def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
     if request.method == 'POST':
-        form = RoomForm(request.POST)
+        form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
             return redirect('home')
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':room})
